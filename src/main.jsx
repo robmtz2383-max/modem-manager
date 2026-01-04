8aa6ce9e6eea11f63f6023c1aff97d7bbc0029cd
robmtz2383-max <robmtz2383@gmail.com>
2026-01-04 14:04:59 -0600

    fix: add vite as dev dependency

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-document.body.innerHTML = '<h1 style="color:red">HTML CARGA</h1>';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <h1 style={{ color: 'green' }}>REACT FUNCIONA</h1>
+);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
