e16fbbb06ec3128d1ef6893b9e57490c3d29a884
robmtz2383-max <robmtz2383@gmail.com>
2026-01-04 14:03:05 -0600

    fix: add vite as dev dependency

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+document.body.innerHTML = '<h1 style="color:red">HTML CARGA</h1>';
 
-  <ErrorBoundary>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </ErrorBoundary>
 );
